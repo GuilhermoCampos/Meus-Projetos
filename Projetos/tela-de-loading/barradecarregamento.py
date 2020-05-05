fb6b276893e28213c6955aa5ff9bdd0ad1e04e65
Guilherme Campos <GuilhermoCampos@users.noreply.github.com>
2020-05-05 19:11:06 -0300

    Organização

---
+++ Projetos/tela-de-loading/barradecarregamento.py
@@ -0,0 +1,18 @@
+import os, time
+c = 0
+while c < 57:
+    os.system('cls')
+    cheio = "■" * c
+    vazio = "□"* (56 - c)
+    print('╔', end='', flush=True)
+    print('═' * 58, end='', flush=True)
+    print('╗', flush=True)
+
+    print(f'║ {cheio}{vazio} ║', flush=True)
+
+    print('╚', end='', flush=True)
+    print('═' * 58, end='', flush=True)
+    print('╝', flush=True)
+    c += 1
+    time.sleep(0)
+input('Enter pra continuar')
